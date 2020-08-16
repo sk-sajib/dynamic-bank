5945e8e7d85924c5ce45be7376aacccde1d7f417
sk-sajib <sksajib0000@gmail.com>
2020-08-16 17:47:32 +0600

    system update

--- script.js
+++ script.js
@@ -1,65 +1,63 @@
+// Login Button Event Handeler
 
-
-// Login button event listener
-const loginBtn = document.getElementById("login")
-loginBtn.addEventListener("click", function(){
-    const loginArea = document.getElementById("login-area")
-    loginArea.style.display = "none"
-    const dasArea = document.getElementById("dashboard-area")
-        dasArea.style.display = "block"
+const loginBtn = document.getElementById('login')
+loginBtn.addEventListener('click', function () {
+    const loginArea = document.getElementById('login-area')
+    loginArea.style.display = 'none'
+    const transArea = document.getElementById('transaction-area')
+    transArea.style.display = 'block'
 })
 
 
-// Deposite button event listener
-
-const depositeBtn = document.getElementById("addDeposite")
-depositeBtn.addEventListener("click",function(){
-
-    const depositeNumber = getInputNumber('addAmount')
-    
-    // const addDepositeAmount = document.getElementById("addAmount").value
-    // const depositeNumber = parseFloat(addDepositeAmount)
-
-    // const currentDeposite = document.getElementById("currentDeposite").innerText
-    // const currentDepositeNumber = parseFloat(currentDeposite)
-    // const totalAmount = depositeNumber + currentDepositeNumber
-    // document.getElementById("currentDeposite").innerText = totalAmount
-
-   
-
-    updateSpanText("currentBalance", depositeNumber)
-    updateSpanText("currentDeposite", depositeNumber)
- 
-    document.getElementById("addAmount").value = " "
-})
-
-// Withdraw button event listener
-
-const withdrawBtn = document.getElementById('addWithdraw')
-withdrawBtn.addEventListener('click', function(){
-    
-    const WithdrawNumber = getInputNumber('withdrawAmount')
-
-    updateSpanText ('currentWithdraw', WithdrawNumber)
-    updateSpanText("currentBalance", -1* WithdrawNumber)
-
-    document.getElementById("withdrawAmount").value = " "
-    
-})
-
-function getInputNumber(id) {
-    const amount = document.getElementById(id).value
+function getInputText(id) {
+    const amount = document.getElementById(id).value | 0
     const amountNumber = parseFloat(amount)
     return amountNumber
-
 }
 
 
-
-function updateSpanText(id,addedNumber){
+function updateSpanText(id, addedNumber) {
     const current = document.getElementById(id).innerText
-    const currentNumber = parseFloat(current)
-    const totalAmount = currentNumber + addedNumber
+    const currentAmount = parseFloat(current)
+    const totalAmount = currentAmount + addedNumber
     document.getElementById(id).innerText = totalAmount
 }
 
+
+// Deposit Button Event Handeler
+const depositBtn = document.getElementById('addDeposit')
+depositBtn.addEventListener('click', function () {
+
+    const depositAmount = getInputText('depositAmount')
+
+    if (depositAmount == 0 || depositAmount < 0) {
+        alert('deposit amount can not be less than zero or blank')
+    } else {
+        updateSpanText('currentDeposit', depositAmount)
+        updateSpanText('currentBalance', depositAmount)
+
+    }
+
+
+    document.getElementById('depositAmount').value = ""
+
+})
+
+// Withdraw Button Event Handeler
+
+const withdrawBtn = document.getElementById('addwithDraw')
+withdrawBtn.addEventListener('click', function () {
+
+    const withdrawAmount = getInputText('withdrawAmount')
+
+    if (withdrawAmount == 0 || withdrawAmount < 0) {
+        alert('withdraw amount can not be less than zero or blank')
+    } else {
+        updateSpanText('currentWithdraw', withdrawAmount)
+        updateSpanText('currentBalance', -1 * withdrawAmount)
+    }
+
+
+    document.getElementById('withdrawAmount').value = ""
+
+})
